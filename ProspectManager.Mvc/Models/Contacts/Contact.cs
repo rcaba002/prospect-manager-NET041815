46f58b91302262156a8fccd0d7a0bfcf58acbce9
Johnny Sysenkham <jsysenkham@comcast.net>
2015-06-09 16:10:38 -0500

    No email dupes issue #15

--- ProspectManager.Mvc/Models/Contacts/Contact.cs
+++ ProspectManager.Mvc/Models/Contacts/Contact.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProspectManager.Mvc.Models.Contacts
 {
-    public class Contact
+    public class Contact : Attribute
     {
         public int Id { get; set; }
 
         [MaxLength(150)]
         [Required]
         public string Name { get; set; }
-
+        [Index("IX_Email",IsUnique = true)]
         [MaxLength(250)]
         [EmailAddress]
         public string Email { get; set; }
 
         public DateTime BirthDate { get; set; }
+
+        public static void Dupes(string dupe)
+        {
+            
+        }
     }
 }
